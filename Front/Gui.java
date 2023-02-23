f579a1cf3e19e4d73a7064af70d128166c31fedc
GIDEON BAUER (836902) <gideonbauer999@gmail.com>
2023-02-23 10:16:21 -0500

    Update Gui.java

---
+++ Front/Gui.java
@@ -0,0 +1,27 @@
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
+import java.awt.Color;
+import javax.swing.*;
+import java.lang.Math;
+
+public class Gui(){
+    
+    private static final int WINDOW_WIDTH = 410;
+    private static final int WINDOW_HEIGHT = 600;
+    private static final int BUTTON_WIDTH = 80;
+    private static final int BUTTON_HEIGHT = 70;
+    private static final int MARGIN_X = 20;
+    private static final int MARGIN_Y = 60;
+
+    private JFrame window; // Main window
+    private JTextField inText; // Input
+
+    window = new JFrame("Calculator");
+    window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    window.setLocationRelativeTo(null); // Move window to center
+
+}
